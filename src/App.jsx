0a32a3597ab3b0b20523bd40617bcc7171331c61
Nathan Vasquez <nathanvasquezdo1@gmail.com>
2024-09-05 20:42:33 -0700

    Add toggle.

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,12 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import { Card } from './components/card'
 import { jsx } from 'react/jsx-runtime'
+import Switch from '@mui/material/Switch';
+import { FormControlLabel } from '@mui/material'
 
 function App() {
   const [data, setData] = useState([]);
+  const [toggle, setToggle] = useState(false)
   const API_URL = import.meta.env.API_URL;
   console.log(API_URL);
   
@@ -21,8 +24,17 @@ function App() {
     fetchData();
   }, []);
 
+  const handleChange = () => {
+    setToggle(!toggle);
+  }
+
   return (
     <>
+    <div className='hstack'>
+      <p style={{marginRight: '0px'}}>Total</p>
+      <Switch onChange={handleChange}/>
+      <p>Today</p>
+    </div>
     {data.map(summoner => (
       <Card cardprops={{
         imageID: summoner.iconId,
@@ -31,7 +43,10 @@ function App() {
         rank: summoner.rank,
         leaguePoints: summoner.leaguePoints,
         wins: summoner.wins,
-        losses: summoner.losses
+        losses: summoner.losses,
+        pDayWins: summoner.pDayWins,
+        pDayLosses: summoner.pDayLosses,
+        toggle: toggle
       }}></Card>
     ))}
     </>
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -9,11 +9,24 @@ export function Card(props) {
                     <p>{props.cardprops.tier} {props.cardprops.rank} - {props.cardprops.leaguePoints} LP</p>
                 </div>
                 <div className="vstack" style={{maxHeight: "100%", fontSize: '16pt', fontWeight: '500'}}>
-                    <p style={{color: 'rgb(175, 255, 175'}}>W: {props.cardprops.wins}</p>
-                    <p style={{color: 'rgb(255, 120, 120'}}>L: {props.cardprops.losses}</p>
+                    <p style={{color: 'rgb(175, 255, 175'}}>W: {
+                        !props.cardprops.toggle 
+                        ? props.cardprops.wins
+                        : props.cardprops.wins - props.cardprops.pDayWins
+                    }</p>
+                    <p style={{color: 'rgb(255, 120, 120'}}>L: {
+                        !props.cardprops.toggle
+                        ? props.cardprops.losses
+                        : props.cardprops.losses - props.cardprops.pDayLosses
+                    }</p>
+                    
                 </div>
                 <div className="winrate">
-                    <p>{(props.cardprops.wins/(props.cardprops.losses + props.cardprops.wins)).toLocaleString(undefined, {style: 'percent', minimumFractionDigits:0})}</p>
+                    <p>{
+                        !props.cardprops.toggle
+                        ? (props.cardprops.wins/(props.cardprops.losses + props.cardprops.wins)).toLocaleString(undefined, {style: 'percent', minimumFractionDigits:0})
+                        : (Math.min(props.cardprops.wins-props.cardprops.pDayWins/(props.cardprops.losses - props.cardprops.pDayLosses + props.cardprops.wins-props.cardprops.pDayWins)), 0).toLocaleString(undefined, {style: 'percent', minimumFractionDigits:0})
+                    }</p>
 
                 </div>
                 <img className="rankicon" src={API_URL+'ranks/' + props.cardprops.tier + '.png'}></img>
